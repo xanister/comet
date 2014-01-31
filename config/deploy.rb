86dea82ef2366c479004cb30c76d4e96bc19ae19
Nicholas Frees <xanister@hotmail.com>
2014-01-31 20:30:39 +0000

    more setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,19 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'anslemgalaxy'
+set :repo_url, 'git@github.com:xanister/anslemgalaxy.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/anslemgalaxy'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+
+set :user, 'nfrees'
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -32,7 +34,9 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
+
+server "54.84.60.91", :app, :web, :db, :primary => true
 
 namespace :deploy do
 
