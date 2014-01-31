b0846a25b984f9770a5e7a37652bfc3c181e616d
Nicholas Frees <xanister@hotmail.com>
2014-01-31 19:51:15 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anslemgalaxy::Application.config.secret_key_base = '3fa2f59cc78788b44d067b4b863012e733db6e14dc61f39cef7627c356dd84eaaa329ab6f0f741c8e57e08727c09422ea1041e7a4733a80f310ce578ee9cb022'
