4c19dca39cbcb4f7655a7b81fafe1c2197ba9f3c
Nicholas Frees <nicholas_frees@condenast.com>
2014-02-07 13:57:13 -0500

    ...

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -62,13 +62,14 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def project_params
-      params[:project]
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def project_params
+    #params[:project]
+    params.require(:project).permit(:name)
+  end
 end
